38f7336f18c948d0c6bcdca2746cbbce1d3202e8
Zoé Sob <zocasob6@yahoo.de>
2021-11-25 16:06:09 +0100

    fehlendes src Verzeichnis hinzugefügt

---
+++ CryptoCroc/src/main/java/controller/PanelCreator.java
@@ -0,0 +1,18 @@
+import javax.swing.JPanel;
+import javax.swing.JButton;
+
+
+public class PanelCreator {
+
+	public JPanel panel = new JPanel(new FlowLayout(SwingConstants.LEADING, 10, 10));
+	
+	public PanelCreator(JPanel panel) {
+		this.panel = panel;
+		
+	}
+	
+	
+	public create(JTextField text) {
+		// TODO
+	}
+}
