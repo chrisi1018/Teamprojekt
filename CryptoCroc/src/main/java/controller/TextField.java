f149290b45ce2a80da3dbfa2c5ef933f8fb54966
Zoé Sob <zocasob6@yahoo.de>
2021-11-25 23:05:05 +0100

    Skelett für ein TextField

---
+++ CryptoCroc/src/main/java/controller/TextField.java
@@ -0,0 +1,28 @@
+package controller;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+/**
+ * Beschreibt ein TextField in CryptoCroc
+ * 
+ * @author zes
+ * @version 1.0
+ */
+public class TextField {
+
+	private JTextField text;
+	private JLabel label;
+
+	public TextField() {
+
+	}
+
+	public JPanel createTextfieldPanel() {
+		return null;
+	}
+
+}
