a448d7dc166237284594d45db05b3bba80043a26
Julian Singer <juli1101@yahoo.de>
2021-11-28 19:31:37 +0100

    Methode createMenuBar durch Konstruktor ersetzt

--- CryptoCroc/src/main/java/controller/Menu.java
+++ CryptoCroc/src/main/java/controller/Menu.java
@@ -8,34 +8,24 @@ import javax.swing.JMenuItem;
  * Beschreibt Aufbau und Funktion der Menueleiste in CryptoCroc
  * 
  * @author Julian Singer
- * @version 1.0
+ * @version 1.1
  */
 public class Menu {
 	
 	private JMenuBar menuBar;
 	
 	/**
-	 * Fügt der Menueleiste des aktuellen Objekts alle übergebenen Menuepunkte zur Auswahl hinzu
+	 * Konstruktor, der Menuepunkte entgegennimmt und diese der Menueleiste hinzufügt
 	 * 
-	 * @param menus auswählbare Menuepunkte
-	 * @return neue Menueleiste
+	 * @param menus zu hinzufügende Menuepunkte
 	 */
-	public JMenuBar createMenuBar(String[] menus) {
+	
+	public Menu(String[] menus) {
 		JMenu menu = new JMenu("Menu");
 		menuBar.add(menu);
 		for (int i=0; i<=menus.length; i++) {
 			JMenuItem item = new JMenuItem(menus[i]);
 			menu.add(item);
 		}
-		return menuBar;
 	}
-	
-	/**
-	 * Fügt der Menueleiste einen einzelnen neuen Menuepunkt hinzu
-	 * @param newMenuItem neuer Menuepunkt
-	 */
-	public void addMenuItem(String newMenuItem) {
-		menuBar.add(new JMenuItem(newMenuItem));
-	}
-
 }
