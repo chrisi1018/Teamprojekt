a946466f93385ddc05722e82f80c1e95c4bdb14e
Zoé Sob <zocasob6@yahoo.de>
2021-12-01 20:47:47 +0100

    Schriftgröße und Art geändert

--- CryptoCroc/src/main/java/controller/TextField.java
+++ CryptoCroc/src/main/java/controller/TextField.java
@@ -1,6 +1,7 @@
 package controller;
 
 import java.awt.BorderLayout;
+import java.awt.Font;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -24,6 +25,7 @@ public class TextField {
 	 */
 	public TextField(String name) {
 		label = new JLabel(name);
+		text.setFont(new Font(Font.DIALOG, Font.PLAIN, 20));
 	}
 
 	/**
