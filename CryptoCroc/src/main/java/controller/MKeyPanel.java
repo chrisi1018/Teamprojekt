abeb9744655394a39f916a00d549e003d973b1cb
Zoé Sob <zocasob6@yahoo.de>
2021-12-09 16:07:11 +0100

    JavaDoc und Kommentare hinzugefügt

--- CryptoCroc/src/main/java/controller/MKeyPanel.java
+++ CryptoCroc/src/main/java/controller/MKeyPanel.java
@@ -1,7 +1,6 @@
 package controller;
 
 import javax.swing.BoxLayout;
-import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
@@ -18,12 +17,12 @@ import java.awt.GridLayout;
  */
 public class MKeyPanel extends KeyPanel {
 
-	private int alphabetSize = 26;
+	private final int alphabetSize = 26;
 	private JTextField[] keys = new JTextField[alphabetSize];
 	private JLabel[] names = new JLabel[alphabetSize];
 
 	/**
-	 * Konstruktor, der der neuen Instanz eine monoalphabetische Verschlüsselung
+	 * Konstruktor, der der neuen Instanz eine monoalphabetische Verschluesselung
 	 * zuordnet
 	 * 
 	 */
@@ -34,43 +33,43 @@ public class MKeyPanel extends KeyPanel {
 
 	@Override
 	public JPanel createKeyPanel() {
+		// Alphabet in Array schreiben
 		this.initNames();
-		BorderLayout bLayout = new BorderLayout();
+
+		// erstellt Panel mit JLabel als titel
+		JPanel title = new JPanel();
 		JLabel description = new JLabel("Schl\u00fcssel");
-		JPanel text = new JPanel();
-		text.setLayout(new BoxLayout(text, BoxLayout.PAGE_AXIS));
+		title.setLayout(new BoxLayout(title, BoxLayout.PAGE_AXIS));
 		description.setAlignmentX(Component.CENTER_ALIGNMENT);
 		description.setFont(new Font(Font.DIALOG, Font.BOLD, 20));
-		text.add(description, BorderLayout.NORTH);
+		title.add(description, BorderLayout.SOUTH);
 
+		// intialisiert Eintraege von keys und setzt Textstil
 		for (int i = 0; i < alphabetSize; i++) {
 			names[i].setFont(new Font(Font.DIALOG, Font.BOLD, 15));
 			keys[i] = new JTextField();
 			keys[i].setFont(new Font(Font.DIALOG, Font.BOLD, 15));
 		}
 
+		// grid für die Buchstaben und Textfelder
 		GridLayout gLayout = new GridLayout(10, 6, 5, 5);
-
 		JPanel inputPanel = new JPanel(gLayout);
 		for (int i = 0; i < alphabetSize; i++) {
 			inputPanel.add(names[i]);
-			keys[i].setColumns(1);
 			inputPanel.add(keys[i]);
 		}
-		JPanel keyPanel = new JPanel(new GridLayout(3, 1));
-		JPanel filler = new JPanel();
-		filler.setSize(5, 5);
-		keyPanel.add(text, BorderLayout.PAGE_START);
-		//keyPanel.add(filler);
-		keyPanel.add(inputPanel, BorderLayout.CENTER);
-	//	keyPanel.add(filler);
-		keyPanel.add(this.createButtonPanel());
 
-		return keyPanel;
+		// alles zusammenfügen und Buttons dazu machen
+		JPanel total = new JPanel(new GridLayout(3, 1));
+		total.add(title, BorderLayout.PAGE_START);
+		total.add(inputPanel, BorderLayout.CENTER);
+		total.add(this.createButtonPanel());
+
+		return total;
 	}
 
 	/**
-	 * Methode zum Initialisieren der Namen der Textfelder
+	 * Methode zum Initialisieren der Namen der Textfelder (Alphabet)
 	 */
 	private void initNames() {
 		for (int i = 0; i < alphabetSize; i++) {
