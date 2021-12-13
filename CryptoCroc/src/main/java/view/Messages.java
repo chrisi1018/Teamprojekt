3fab55bb2fae724ad94f720125dae503cf535fd8
Julian Sturm <sturm-julian@gmx.de>
2021-12-13 20:11:18 +0100

    Wahrnnachricht angepasst, Errornachricht hinzugefügt, JaNeinFrage hinzugefügt

--- CryptoCroc/src/main/java/view/Messages.java
+++ CryptoCroc/src/main/java/view/Messages.java
@@ -1,10 +1,9 @@
 package view;
 
-import java.awt.Component;
 import javax.swing.JOptionPane;
 
 /**
- * Eine Hilfsklasse die Methoden für die Ausgabe von Warn- und Errormitteilungen bereit stellt.
+ * Eine Hilfsklasse die Methoden für die Ausgabe von Pop-Up Meitteilungen bereit stellt.
  * 
  * @author Julian
  * @version 1.0
@@ -19,12 +18,36 @@ public final class Messages {
 	/**
 	 * Erzeugt ein Pop-up Fenster mit einer Fehlermeldung
 	 * 
-	 * @param message Die Fehlermeldung
-	 * @param parent Die Componente durch den Warnhinweis blockiert wird.
+	 * @param message die Fehlermeldung
 	 */
-	public static void errorMessage(String message, Component parent) {
-		JOptionPane.showOptionDialog(parent, "Test", "Fehler", JOptionPane.YES_OPTION,
-				JOptionPane.WARNING_MESSAGE, null, new String[]{"A", "B", "C"}, "B");
+	public static void errorMessage(String message) {
+		JOptionPane.showOptionDialog(null, message, "Achtung", JOptionPane.OK_OPTION,
+				JOptionPane.ERROR_MESSAGE, null, new String[] {"OK"}, "OK");
 	}
-
+	
+	/**
+	 * Erzeugt eine Pop-Up Fenster mit einem Wahnhinweis.
+	 * 
+	 * @param message Die Warnnachricht die AUsgegeben wird.
+	 */
+	public static void warningMessage(String message) {
+		JOptionPane.showOptionDialog(null, message, "Achtung", JOptionPane.OK_OPTION,
+				JOptionPane.WARNING_MESSAGE, null, new String[] {"OK"}, "OK");
+	}
+	
+	/**
+	 * Erzeugt eine Pop-Up Abfrage für eine Ja oder Nein Frage
+	 * 
+	 *@param message die Angezeigt Frage
+	 *@return ein boolscher Wert ob die Frage mit Ja beantwortet wurde
+	 */
+	public static boolean yesNoQuestion(String message) {
+		int temp = JOptionPane.showOptionDialog(null, message, "Achtung",
+				JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.WARNING_MESSAGE,
+				null, new String[] {"JA", "Nein"}, "Nein");
+		if (temp == JOptionPane.YES_OPTION) {
+			return true;
+		}
+		return false;
+	}
 }
