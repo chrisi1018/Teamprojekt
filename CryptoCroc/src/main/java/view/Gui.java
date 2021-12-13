7f8162d57b6ce5a9cb2f249245b404c4bf7a6f05
Julian Sturm <sturm-julian@gmx.de>
2021-12-13 22:45:14 +0100

    Änderungen an der Klasse Gui, Erstellung einer repaintFrame Methode

--- CryptoCroc/src/main/java/view/Gui.java
+++ CryptoCroc/src/main/java/view/Gui.java
@@ -82,21 +82,18 @@ public class Gui {
 	 */
 	public void setKeyPanel(JPanel keyPanel) {
 		this.mainPanel.remove(this.keyPanel);
-		this.mainPanel.revalidate();
-		this.mainPanel.repaint();
 		this.keyPanel = keyPanel;
 		this.mainPanel.add(this.keyPanel, BorderLayout.CENTER);
 		this.keyPanel.setPreferredSize(new Dimension(200, 740));
-		this.mainPanel.revalidate();
-		this.mainPanel.repaint();
+		this.repaintFrame();
 	}
 	
 	/**
-	 * Getter-Methode für den Frame
+	 * Methodie die den Frame repainted, muss nach veränderungen der Gui aufgerufen werden
 	 * 
-	 * @return Der Frame der Gui
 	 */
-	public JFrame getFrame() {
-		return this.frame;
+	public void repaintFrame() {
+		this.mainPanel.revalidate();
+		this.mainPanel.repaint();
 	}
 }
