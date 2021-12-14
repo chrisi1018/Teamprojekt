3ef0d9a778dbd1bcd79a4c000dc2185ef202521f
Zoé Sob <zocasob6@yahoo.de>
2021-12-14 18:56:35 +0100

    Crypt Klasse für monoalphabetisch

---
+++ CryptoCroc/src/main/java/model/MCrypt.java
@@ -0,0 +1,19 @@
+package model;
+
+/**
+ * */
+public class MCrypt extends Crypt {
+
+	@Override
+	public String cryptAll(String plainText, String key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String decryptAll(String cryptoText, String key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
