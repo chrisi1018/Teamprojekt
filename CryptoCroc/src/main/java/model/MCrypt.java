12ffacf939522d2949406edc8e80d99a177067bd
Zoé Sob <zocasob6@yahoo.de>
2021-12-15 16:31:25 +0100

    Methoden die einen Text verschlüsseln hinzugefügt

--- CryptoCroc/src/main/java/model/MCrypt.java
+++ CryptoCroc/src/main/java/model/MCrypt.java
@@ -1,19 +1,73 @@
 package model;
 
 /**
- * */
+ * Fuer eine monoalphabetische Verschluesselung
+ * 
+ * @author zes
+ * @version 1.0
+ */
 public class MCrypt extends Crypt {
+
+	/**
+	 * Macht aus einem Schluesselwort ein Array
+	 * 
+	 * @param key das Schluesselwort als String
+	 * @return ein Array mit in jedem Slot ein Buchstabe
+	 */
+	private String[] convertKeyToArray(String key) {
+		String[] keyArray = new String[key.length()];
+		for (int i = 0; i < keyArray.length; i++) {
+			keyArray[i] = Character.toString(key.charAt(i));
+		}
+		return keyArray;
+	}
 
 	@Override
 	public String cryptAll(String plainText, String key) {
-		// TODO Auto-generated method stub
-		return null;
+		String[] keyArray = this.convertKeyToArray(key);
+		String cryptoText = "";
+		for (int i = 0; i < plainText.length(); i++) {
+			cryptoText += this.cryptChar(plainText.charAt(i), keyArray);
+		}
+		return cryptoText;
+	}
+
+	/**
+	 * Verschluesselt einen Buchstaben mit einem gegebenen Schluessel, indem der
+	 * Wert an der Stelle 'letter' aus dem Schluessel-Array abgelesen wird
+	 * 
+	 * @param letter der aktuelle Buchstabe, der verschluesselt werden soll
+	 * @param key    ein Array mit dem aktuellen Schluessel
+	 * @return der verschluesselte Buchstabe als String
+	 */
+	private String cryptChar(char letter, String[] key) {
+		String val = "";
+		if (letter >= 'a' && letter <= 'z') {
+			val = key[(letter) % 'a'];
+		} else if (letter >= 'A' && letter <= 'Z') {
+			val = key[(letter) % 'A'];
+		}
+		return val;
 	}
 
 	@Override
 	public String decryptAll(String cryptoText, String key) {
-		// TODO Auto-generated method stub
-		return null;
+		String[] keyArray = this.convertKeyToArray(key);
+		String plainText = "";
+		for (int i = 0; i < cryptoText.length(); i++) {
+			plainText += this.decryptChar(cryptoText.charAt(i), keyArray);
+		}
+		return plainText;
+	}
+
+	/**
+	 * @param letter
+	 * @param key
+	 * @return String
+	 */
+	private String decryptChar(char letter, String[] key) {
+		String val = "";
+		return val;
 	}
 
 }
