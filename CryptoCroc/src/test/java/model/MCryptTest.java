d136753f218ff3e62bafdf4519fc5c981c21be4e
Zoé Sob <zocasob6@yahoo.de>
2022-01-13 10:59:12 +0100

    Testklasse org.junit.jupiter.api.Assertions.assertTrue importiert statt org.junit.Assert.assertTrue

--- CryptoCroc/src/test/java/model/MCryptTest.java
+++ CryptoCroc/src/test/java/model/MCryptTest.java
@@ -1,6 +1,6 @@
 package model;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.Random;
