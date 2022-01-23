ecfcbb3f2d237bd120d71888fbaeec3257f469d1
Julian Sturm <sturm-julian@gmx.de>
2022-01-23 17:54:43 +0100

    zwischenspeichern

--- CryptoCroc/src/frequencyanalysis/java/controller/FAController.java
+++ CryptoCroc/src/frequencyanalysis/java/controller/FAController.java
@@ -1,5 +1,22 @@
 package controller;
 
-public class FAController {
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import javax.swing.JButton;
 
+public class FAController {
+	
+	private KeyPanel key;
+	private JLabel title;
+	private JTextField lengthText;
+	private JCheckBox monoCheckBox;
+	private JComboBox language;
+	private JComboBox keyChar;
+	private JButton left;
+	private JButton right;
+	//private FAAlgo algo;
+	//private FATable table;
+	//private 
 }
