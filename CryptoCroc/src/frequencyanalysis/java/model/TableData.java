53060086c58fa2512ad384a9ea9b98cd9fe9359c
chrisi1018 <chis.blach@web.de>
2022-01-26 19:14:33 +0100

    Initalisieren der Klasse TableData (T12)

--- CryptoCroc/src/frequencyanalysis/java/model/TableData.java
+++ CryptoCroc/src/frequencyanalysis/java/model/TableData.java
@@ -1,5 +1,30 @@
 package model;
 
 public class TableData {
-
+	private float per;
+	private char[] textFieldChar[];
+	
+	public TableData(float per[][]) {
+		
+	}
+	
+	public float[] getPer(int i) {
+		return null;
+	}
+	
+	public void setTextFieldChar(char c, int i) {
+		
+	}
+	
+	public char getTextFieldChar(int i) {
+		return ' ';
+	}
+	
+	public void initTextFieldChar() {
+		
+	}
+	
+	public float[][] getForGraph() {
+		return null;
+	}
 }
