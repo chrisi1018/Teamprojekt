1ef898f2b5ee507adccc984d84755b2fbd3edd7c
Zoé Sob <zocasob6@yahoo.de>
2022-01-27 12:57:09 +0100

    Klasse an Klassendiagramm angepasst

--- CryptoCroc/src/frequencyanalysis/java/controller/FAGraph.java
+++ CryptoCroc/src/frequencyanalysis/java/controller/FAGraph.java
@@ -1,5 +1,46 @@
 package controller;
 
+import javax.swing.JPanel;
+
+/**
+ * Erstellt die Grafik zur Buchstabenverteilung der Haeufigkeitsanalyse
+ * 
+ * @author zes
+ * @version 1.0
+ */
 public class FAGraph {
 
+	private JPanel graphPanel;
+
+	/**
+	 * Konstruktor fuer einen FAGraph, erstellt das GraphPanel
+	 */
+	public FAGraph() {
+		createGraphPanel();
+	}
+
+	/**
+	 * Erstellt das GraphPanel
+	 */
+	public void createGraphPanel() {
+
+	}
+
+	/**
+	 * Getter fuer das GraphPanel
+	 * 
+	 * @return das graphPanel
+	 */
+	public JPanel getGraphPanel() {
+		return graphPanel;
+	}
+
+	/**
+	 * Setter fuer das GraphPanel
+	 * 
+	 * @param graphPanel das neue graphPanel
+	 */
+	public void setGraphPanel(JPanel graphPanel) {
+		this.graphPanel = graphPanel;
+	}
 }
