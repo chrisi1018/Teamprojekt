6ec765c48de4644b4038a7b5c6412cb92b382a2f
Zoé Sob <zocasob6@yahoo.de>
2022-02-01 14:13:49 +0100

    Methodennamen korrigiert; textfield size static gemacht (T9)

--- CryptoCroc/src/frequencyanalysis/java/model/TableData.java
+++ CryptoCroc/src/frequencyanalysis/java/model/TableData.java
@@ -1,36 +1,38 @@
 package model;
 
 /**
- * Die Klasse verwaltet den Haeufigkeitsprozentsatz und die Buchstaben-Reihenfolge des Textfelds.
+ * Die Klasse verwaltet den Haeufigkeitsprozentsatz und die
+ * Buchstaben-Reihenfolge des Textfelds.
  * 
  * @author chrisi
  * @version 1.0
  *
  */
 public class TableData {
-	final private int TEXTFIELD_SIZE = 26;
-	
+	private static final int TEXTFIELD_SIZE = 26;
+
 	private float[] frequencyPercentage = new float[TEXTFIELD_SIZE];
 	private char[] textFieldChar = new char[TEXTFIELD_SIZE];
-	
+
 	/**
 	 * Der Konstruktor fuer die Klasse TableData, speichert die Prozentzahlen ab.
 	 * 
-	 * @param frequencyPercentage die Prozentzahlen fuer die Haefigkeitsanalyse werden uebergeben.
+	 * @param frequencyPercentage die Prozentzahlen fuer die Haefigkeitsanalyse
+	 *                            werden uebergeben.
 	 */
 	public TableData(float[] frequencyPercentage) {
 		this.frequencyPercentage = frequencyPercentage;
 	}
-	
+
 	/**
 	 * Gibt die Prozentzahlen der Analyse zurueck.
 	 * 
 	 * @return den Haeufigkeitsprozentsatz
 	 */
-	public float[] getFrequencyPercantage() {
+	public float[] getFrequencyPercentage() {
 		return this.frequencyPercentage;
 	}
-	
+
 	/**
 	 * Belegt das i-te Feld in den Char-Textfeldern neu.
 	 * 
@@ -40,7 +42,7 @@ public class TableData {
 	public void setTextFieldChar(char c, int i) {
 		this.textFieldChar[i] = c;
 	}
-	
+
 	/**
 	 * Gibt das i-te Feld in den Char-Textfeldern zurueck.
 	 * 
@@ -50,7 +52,7 @@ public class TableData {
 	public char getTextFieldChar(int i) {
 		return this.textFieldChar[i];
 	}
-	
+
 	/**
 	 * Initalisiert das Textfeld-Array mit den Werten A-Z
 	 */
@@ -60,19 +62,20 @@ public class TableData {
 			this.textFieldChar[i] = (char) (letter + i);
 		}
 	}
-	
+
 	/**
-	 * Gibt die Haeufigkeitsprozentsatz sortiert nach der Anordnung der Buchstaben in den Textfeldern zurueck.
+	 * Gibt die Haeufigkeitsprozentsatz sortiert nach der Anordnung der Buchstaben
+	 * in den Textfeldern zurueck.
 	 * 
 	 * @return den sortierten Haeufigkeitsprozentsatz
 	 */
 	public float[] getForGraph() {
-		float[] sortedFrequencyPercentage = new float[this.TEXTFIELD_SIZE];
+		float[] sortedFrequencyPercentage = new float[TableData.TEXTFIELD_SIZE];
 		char letter = 'A';
-		
-		for (int i = 0; i < this.textFieldChar.length; i++) { //Geht alle Buchstaben von A-Z durch
-			for (int j = 0; j < this.textFieldChar.length; j++) { //Geht alle eintraege in dem Textfeld-Array durch
-				if (this.textFieldChar[j] == (char) (letter + i)) { //Vertauscht die Prozentzahlen falls im 
+
+		for (int i = 0; i < this.textFieldChar.length; i++) { // Geht alle Buchstaben von A-Z durch
+			for (int j = 0; j < this.textFieldChar.length; j++) { // Geht alle eintraege in dem Textfeld-Array durch
+				if (this.textFieldChar[j] == (char) (letter + i)) { // Vertauscht die Prozentzahlen falls im
 					sortedFrequencyPercentage[j] = this.frequencyPercentage[i];
 				}
 			}
