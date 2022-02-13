a8f6cecee296b937bf8371c13fa68cdc2c9c4ac3
Zoé Sob <zocasob6@yahoo.de>
2022-02-13 19:28:58 +0100

    Rechtsklick für macos aktiviert (US20)

--- CryptoCroc/src/main/java/controller/MousePopupMenu.java
+++ CryptoCroc/src/main/java/controller/MousePopupMenu.java
@@ -3,6 +3,7 @@ package controller;
 import javax.swing.JMenuItem;
 import javax.swing.JPopupMenu;
 import javax.swing.JTextArea;
+import javax.swing.UIManager;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -10,33 +11,41 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 public class MousePopupMenu {
-    private JPopupMenu menu = new JPopupMenu();
-    private JMenuItem entry = null;
+	private JPopupMenu menu = new JPopupMenu();
+	private JMenuItem entry = null;
+	// standardwert bei rechtsklick auf macos
+	private final int macOSMouseClick = 3;
 
-    public MousePopupMenu(JTextArea textArea) {
-        ActionListener al = new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                if(textArea.getToolkit().getSystemClipboard() != null) {
-                    if(e.getActionCommand().equals("Ausschneiden")) {
-                        textArea.cut();
-                    } else if(e.getActionCommand().equals("Kopieren")) {
-                        textArea.copy();
-                    } else if(e.getActionCommand().equals("Einf\u00fcgen")) {
-                        textArea.paste();
-                    } else if(e.getActionCommand().equals("Alles markieren")) {
-                        textArea.selectAll();
-                    }
-                }
-            }
-        };
+	public MousePopupMenu(JTextArea textArea) {
+		ActionListener al = new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (textArea.getToolkit().getSystemClipboard() != null) {
+					if (e.getActionCommand().equals("Ausschneiden")) {
+						textArea.cut();
+					} else if (e.getActionCommand().equals("Kopieren")) {
+						textArea.copy();
+					} else if (e.getActionCommand().equals("Einf\u00fcgen")) {
+						textArea.paste();
+					} else if (e.getActionCommand().equals("Alles markieren")) {
+						textArea.selectAll();
+					}
+				}
+			}
+		};
 
-        textArea.addMouseListener(new MouseAdapter() {
-        	@Override
-            public void mouseReleased(MouseEvent me) {
-                if (me.isPopupTrigger()) {
-                    menu.show(me.getComponent(), me.getX(), me.getY());
-                }
-            }
+		textArea.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseReleased(MouseEvent me) {
+				// spezialfall fuer macOS
+				if (UIManager.getSystemLookAndFeelClassName().equals("com.apple.laf.AquaLookAndFeel")) {
+					if (me.getButton() == macOSMouseClick) {
+						menu.show(me.getComponent(), me.getX(), me.getY());
+					}
+				}
+				if (me.isPopupTrigger()) {
+					menu.show(me.getComponent(), me.getX(), me.getY());
+				}
+			}
 
 			@Override
 			public void mouseClicked(java.awt.event.MouseEvent e) {
@@ -57,25 +66,25 @@ public class MousePopupMenu {
 			public void mouseExited(java.awt.event.MouseEvent e) {
 				// tue nichts
 			}
-        });
+		});
 
-        entry = new JMenuItem("Ausschneiden");
-        menu.add(entry);
-        entry.addActionListener(al);
+		entry = new JMenuItem("Ausschneiden");
+		menu.add(entry);
+		entry.addActionListener(al);
 
-        entry = new JMenuItem("Kopieren");
-        menu.add(entry);
-        entry.addActionListener(al);
+		entry = new JMenuItem("Kopieren");
+		menu.add(entry);
+		entry.addActionListener(al);
 
-        entry = new JMenuItem("Einf\u00fcgen");
-        menu.add(entry);
-        entry.addActionListener(al);
+		entry = new JMenuItem("Einf\u00fcgen");
+		menu.add(entry);
+		entry.addActionListener(al);
 
-        menu.addSeparator();
+		menu.addSeparator();
 
-        entry = new JMenuItem("Alles markieren");
-        menu.add(entry);
-        entry.addActionListener(al);
+		entry = new JMenuItem("Alles markieren");
+		menu.add(entry);
+		entry.addActionListener(al);
 
-    }
+	}
 }
