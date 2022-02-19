af807f86686859ec167c47e2123fa0cdf69ea7d3
Julian Singer <julian.singer@mail.de>
2022-02-19 10:09:04 +0100

    Imports in Menu übersichtlicher gemacht

--- CryptoCroc/src/main/java/controller/Menu.java
+++ CryptoCroc/src/main/java/controller/Menu.java
@@ -6,6 +6,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.Scanner;
+
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
@@ -13,6 +14,7 @@ import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.filechooser.FileNameExtensionFilter;
+
 import org.apache.commons.io.FilenameUtils;
 
 import view.Messages;
