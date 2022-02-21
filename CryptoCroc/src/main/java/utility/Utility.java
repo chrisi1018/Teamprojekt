e7c721ae28cab3bd9c2f08cafe812632c9deaaff
Zoé Sob <zoe.sobota14@gmail.com>
2022-02-21 14:42:32 +0100

    JavaDocs hinzugefügt und neue Konstante für maximale Anzahl Fehlermeldungen (T34)

--- CryptoCroc/src/main/java/utility/Utility.java
+++ CryptoCroc/src/main/java/utility/Utility.java
@@ -9,34 +9,53 @@ import javax.swing.border.Border;
 /**
  * Hilfsklasse, die alle benutzten Konstanten zur Verfuegung stellt
  * 
- * @author Julian Singer, chrisi
+ * @author Julian Singer, chrisi, zes
  * @version 1.3
  *
  */
-public class Utility {
-	
-	public static final int ALPHABET_SIZE = 26;     // Groesse des Alphabets
-	public static final int MAXIMUM_KEY_LENGTH = 15; // maximale Schluessellaenge
-	public static final int KEY_LENGTH_DIGITS = 2;   // Schluessellaenge kann maximal dreistellig sein
-	public static final int KEY_LENGTH_FOR_SINGLE_SHIFT = 1; // Schluessellaenge fuer Caesar und Monoalphabetisch
-	
-	public static final char FIRST_LETTER = 'A';    // erster Buchstabe des Alphabets
-	
-	public static final Color DARK_GREEN = new Color(75, 115, 14); // gruene Farbe fuer Graph und Farbverlauf
-	public static final Color ORANGE = new Color(185, 137, 0); // orangene Farbe fuer Graph, Menue und Buttons
-	public static final Color LIGHT_GREEN = new Color(191, 205, 169); // hellgruene Farbe fuer den Farbverlauf
-	public static final Color WHITE = new Color(255, 255, 255); // weisse Farbe fuer die Texte bei Farbverlauf
-	
-	public static final Font FONT = new Font("Arial", Font.BOLD, 12); // Standard-Schriftzug fuer die Buttons
-	public static final Font MENU_FONT = new Font("Arial", Font.BOLD, 14); // Standard-Schriftzug fuer das Menue
-	public static final Font TEXT_FONT = new Font("Arial", Font.PLAIN, 12); // Standard-Schriftzug fuer Text in Textfeld
-	public static final Font HEADLINE_LABEL_FONT = new Font("Arial", Font.BOLD, 18); // Standard-Schriftzug fuer 
-																					// Ueberschriften Labels
-	public static final Font LABEL_FONT = new Font("Arial", Font.BOLD, 14); // Standard-Schriftzug fuer Labels
-	
-	// Standard-Umrandung fuer die Textfelder
+public final class Utility {
+	/** Groesse des Alphabets */
+	public static final int ALPHABET_SIZE = 26;
+	/** maximale Schluessellaenge */
+	public static final int MAXIMUM_KEY_LENGTH = 15;
+	/** Schluessellaenge kann maximal zweistellig sein */
+	public static final int KEY_LENGTH_DIGITS = 2;
+	/** Schluessellaenge fuer Caesar und Monoalphabetisch */
+	public static final int KEY_LENGTH_FOR_SINGLE_SHIFT = 1;
+
+	/** erster Buchstabe des Alphabets */
+	public static final char FIRST_LETTER = 'A';
+
+	/** gruene Farbe fuer Graph und Farbverlauf */
+	public static final Color DARK_GREEN = new Color(75, 115, 14);
+	/** orangene Farbe fuer Graph, Menue und Buttons */
+	public static final Color ORANGE = new Color(185, 137, 0);
+	/** hellgruene Farbe fuer den Farbverlauf */
+	public static final Color LIGHT_GREEN = new Color(191, 205, 169);
+	/** weisse Farbe fuer die Texte bei Farbverlauf */
+	public static final Color WHITE = new Color(255, 255, 255);
+
+	/** Standard-Schriftzug fuer die Buttons */
+	public static final Font FONT = new Font("Arial", Font.BOLD, 12);
+	/** Standard-Schriftzug fuer das Menue */
+	public static final Font MENU_FONT = new Font("Arial", Font.BOLD, 14);
+	/** Standard-Schriftzug fuer Text in Textfeld */
+	public static final Font TEXT_FONT = new Font("Arial", Font.PLAIN, 12);
+	/** Standard-Schriftzug fuer Ueberschriften Labels */
+	public static final Font HEADLINE_LABEL_FONT = new Font("Arial", Font.BOLD, 18);
+	/** Standard-Schriftzug fuer Labels */
+	public static final Font LABEL_FONT = new Font("Arial", Font.BOLD, 14);
+
+	/** Standard-Umrandung fuer die Textfelder */
 	public static final Border TEXTFIELD_BORDER = BorderFactory.createMatteBorder(1, 1, 1, 1, Utility.DARK_GREEN);
-	// Umrandung ausgewaeltes Textfeld
+	/** Umrandung ausgewaeltes Textfeld */
 	public static final Border FOCUS_TEXTFIELD_BORDER = BorderFactory.createMatteBorder(1, 1, 1, 1, Utility.ORANGE);
-	
+
+	/** Maximale Anzahl wie oft Fehlermeldungen angezeigt werden */
+	public static final int MAX_ERROR_MESSAGES = 3;
+
+	// privater Konstruktor
+	private Utility() {
+
+	}
 }
